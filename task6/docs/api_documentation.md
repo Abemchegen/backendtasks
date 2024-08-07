741a303c40e75ab633df673b3cd4506019d33325
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2024-08-07 11:10:19 +0300

    task 6 documentation

---
+++ task6/docs/api_documentation.md
@@ -0,0 +1,3 @@
+### Task Manager
+
+**Postman documentation link** : https://documenter.getpostman.com/view/37350482/2sA3rzKCWP
--- task6/models/task.go
+++ task6/models/task.go
@@ -8,7 +8,7 @@ import (
 
 type Task struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	UserID      primitive.ObjectID `bson:"user_id,omitempty" json:"user_id,omitempty"`
+	UserID      primitive.ObjectID `bson:"user_id,omitempty" json:"-"`
 	Title       string             `json:"title"`
 	Description string             `json:"description"`
 	DueDate     time.Time          `json:"duedate"`
